13e8b1ad2658c4be1db38ec65c585dcbfaddc77a
Tobias Hofmann <hofmann.tobias@outlook.com>
2024-10-07 19:19:49 +0200

    add step 5 description

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# replay-ui-tests
-Replay UI tests captured by Chrome recorder using replay
+# Replay UI Tests
+
+This is a sample app to show how replay can be used to automatically run UI tests for testing a web app. More information about the approach and app created as an example can be found at the my blog post [Replay UI tests]().
+
+The recommended approach for going through the example is to read first the blog post and create the app in parallel by following the steps outlined in the blog post. This repository should be used as reference and help.
+
+## Step 5: Run all tests
+
+Add the custom extension to the npm test script in package.json
+
+```json
+"test": "replay --ext extension.js json"
+```
+
+Run all tests in folder json by running npm test.
+
+```sh
+npm test
+```
+
+All tests in the folder JSON should be executed successfully.
+
+Test report:
+
+![Test results](doc/TestResult.png)
---
+++ extension.js
@@ -0,0 +1,37 @@
+import { createRunner, parse, PuppeteerRunnerExtension } from '@puppeteer/replay';
+import puppeteer from 'puppeteer';
+
+export default class Extension extends PuppeteerRunnerExtension {
+    async beforeAllSteps(flow) {
+      if (!this.browser) {
+        this.browser = await puppeteer.launch();
+      }
+      if (!this.page) {
+        this.page = await this.browser.newPage();
+        
+      }
+      await super.beforeAllSteps(flow);
+      console.log(flow.title);
+      console.log('starting');      
+    }
+  
+    async beforeEachStep(step, flow) {
+      await super.beforeEachStep(step, flow);
+      //console.log(step.type);
+      
+      // timeout. Wait for 2 seconds so UI5 loads and the elements are loaded and clickable
+      await new Promise(r => setTimeout(r, 2000));
+    }
+
+
+    async afterEachStep(step, flow) {
+      await super.afterEachStep(step, flow);
+    }
+
+    async afterAllSteps(flow) {
+        await super.afterAllSteps(flow);
+        await this.browser.close();
+    }
+
+    
+  }
